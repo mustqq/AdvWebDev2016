3845fd1bd661ee6cfa4f4ee48e62e3afde7b0497
mustqq <glenmustonen@gmail.com>
2026-04-09 17:59:36 +0300

    Made blocks smaller

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,8 +1,18 @@
-function app()
-{
+import './index.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import About from './components/About';
+import Footer from './components/Footer';
+
+function App() {
   return (
-    <div>
-      <h1>Yo</h1>
+    <div className="d-flex flex-column h-100">
+      <main className="flex-shrink-0">
+        <Navbar />
+        <Header />
+        <About />
+      </main>
+      <Footer />
     </div>
   )
 }
---
+++ final-project/src/components/Header.jsx
@@ -0,0 +1,30 @@
+function Header() {
+  return (
+    <header className="py-5">
+      <div className="container px-5 pb-5">
+        <div className="row gx-5 align-items-center">
+          <div className="col-xxl-5">
+            <div className="text-center text-xxl-start">
+              <div className="badge bg-gradient-primary-to-secondary text-white mb-4"><div className="text-uppercase">Design &middot; Development &middot; Marketing</div></div>
+              <div className="fs-3 fw-light text-muted">I can help your business to</div>
+              <h1 className="display-3 fw-bolder mb-5"><span className="text-gradient d-inline">Get online and grow fast</span></h1>
+              <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
+                <a className="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="resume.html">Resume</a>
+                <a className="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="projects.html">Projects</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-xxl-7">
+            <div className="d-flex justify-content-center mt-5 mt-xxl-0">
+              <div className="profile bg-gradient-primary-to-secondary">
+                <img className="profile-img" src="assets/profile.png" alt="..." />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
