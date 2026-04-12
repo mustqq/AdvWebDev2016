1b8afc9c2de4c8cc699a1ca3414c46f5fff1e992
mustqq <glenmustonen@gmail.com>
2026-04-12 15:16:15 +0300

    .

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,16 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
 import './index.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import About from './components/About';
 import Footer from './components/Footer';
+import FormPage from './pages/FormPage';
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <About />
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="d-flex flex-column h-100">
       <main className="flex-shrink-0">
         <Navbar />
-        <Header />
-        <About />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<FormPage />} />
+        </Routes>
       </main>
       <Footer />
     </div>
